a6f5aaae9b585874e73a1625a6543fb79fa8d419
donelli <eduardopellenz@hotmail.com>
2020-11-27 21:15:35 -0300

    started form validation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var riskLevel = 6;
 
@@ -10,17 +11,50 @@ $(() => {
     
     refreshRiskBoxes();
     
+    $('#main-form').submit((e) => {
+        e.preventDefault();
+        if (!validateForm()) {
+            return true;
+        }
+        return true;
+    });
+    
 });
+
+function validateForm() {
+    
+    $('.is-invalid').removeClass('is-invalid');
+    
+    let error = '';
+    if (error = verifyTextInput('name', 'nome do paciente', 10)) {
+        showErrorInInput(error, 'name');
+        return false;
+    }
+    
+    return true;
+}
+
+function verifyTextInput(elementId, name, minLength = 0) {
+    const element = $('#' + elementId)[0];
+    if (!element || !element.value) {
+        return 'O campo ' + name + ' deve ser preenchido!';
+    }
+    if (element.value.length < minLength) {
+        return 'O campo ' + name + ' deve ter no mínimo ' + minLength + ' caracteres!';
+    }
+    return '';
+}
+
+function showErrorInInput(error, elementId) {
+    const element = $("#" + elementId);
+    element.addClass('is-invalid');
+    alert(error);
+    element.focus();
+}
 
 function resetForm() {
     riskLevel = 0;
     refreshRiskBoxes();
-}
-
-function recalculateRisk() {
-    
-    
-    
 }
 
 function refreshRiskBoxes() {
