0dcb6c27c7a92a374628f5794e16c50a9c0e08ab
donelli <eduardopellenz@hotmail.com>
2020-12-03 22:10:03 -0300

    added required input validation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var riskLevel = 6;
+var riskLevel = 7;
 
 $(() => {
    
@@ -25,36 +25,147 @@ function validateForm() {
     
     $('.is-invalid').removeClass('is-invalid');
     
+    let isValid = true;
+    let firstErrorInput;
+    
     let error = '';
-    if (error = verifyTextInput('name', 'nome do paciente', 10)) {
-        showErrorInInput(error, 'name');
-        return false;
+    if (error = verifyTextInput('name', 10)) {
+        addErrorFeedbackToInput('name', error);
+        firstErrorInput = firstErrorInput ?? 'name';
+        isValid = false;
     }
     
-    return true;
+    if (error = verifyTextInput('cpf')) {
+        addErrorFeedbackToInput('cpf', error);
+        firstErrorInput = firstErrorInput ?? 'cpf';
+        isValid = false;
+    }
+    
+    const cpf = $('#cpf').cleanVal();
+    
+    if (!validateCPF(cpf)) {
+        addErrorFeedbackToInput('cpf', 'CPF inválido!');
+        firstErrorInput = firstErrorInput ?? 'cpf';
+        isValid = false;
+    }
+    
+    if (error = verifyTextInput('adress')) {
+        addErrorFeedbackToInput('adress', error);
+        firstErrorInput = firstErrorInput ?? 'adress';
+        isValid = false;
+    }
+    
+    if (error = verifyTextInput('adress-number')) {
+        addErrorFeedbackToInput('adress-number', error);
+        firstErrorInput = firstErrorInput ?? 'adress-number';
+        isValid = false;
+    }
+    
+    if (error = verifyTextInput('adress-complement')) {
+        addErrorFeedbackToInput('adress-complement', error);
+        firstErrorInput = firstErrorInput ?? 'adress-complement';
+        isValid = false;
+    }
+    
+    if (error = verifyTextInput('adress-city')) {
+        addErrorFeedbackToInput('adress-city', error);
+        firstErrorInput = firstErrorInput ?? 'adress-city';
+        isValid = false;
+    }
+    
+    if (error = verifyTextInput('adress-city')) {
+        addErrorFeedbackToInput('adress-city', error);
+        firstErrorInput = firstErrorInput ?? 'adress-city';
+        isValid = false;
+    }
+    
+    if (error = verifyTextInput('adress-state')) {
+        addErrorFeedbackToInput('adress-state', error);
+        firstErrorInput = firstErrorInput ?? 'adress-state';
+        isValid = false;
+    }
+    
+    if (error = verifyTextInput('adress-cep')) {
+        addErrorFeedbackToInput('adress-cep', error);
+        firstErrorInput = firstErrorInput ?? 'adress-cep';
+        isValid = false;
+    }
+    
+    
+    
+    if (isValid) {
+        alert('formulário válido!');
+        return true;
+    }
+    
+    alert('Foram encontrados erros no formulário!');
+    $('#' + firstErrorInput).focus();
+    
+    return false;
 }
 
-function verifyTextInput(elementId, name, minLength = 0) {
+function verifyTextInput(elementId, minLength = 0) {
     const element = $('#' + elementId)[0];
     if (!element || !element.value) {
-        return 'O campo ' + name + ' deve ser preenchido!';
+        return 'Campo obrigatório!';
     }
     if (element.value.length < minLength) {
-        return 'O campo ' + name + ' deve ter no mínimo ' + minLength + ' caracteres!';
+        return 'Campo deve ter no mínimo ' + minLength + ' caracteres!';
     }
     return '';
 }
 
-function showErrorInInput(error, elementId) {
+function addErrorFeedbackToInput(elementId, error) {
     const element = $("#" + elementId);
     element.addClass('is-invalid');
-    alert(error);
-    element.focus();
+    $('#invalid-feedback-' + elementId).html(error);
+    // alert(error);
+    // element.focus();
 }
 
 function resetForm() {
     riskLevel = 0;
     refreshRiskBoxes();
+}
+
+function validateCPF(c){
+    
+    if (c.length != 11) {
+        return false;
+    }
+    
+    var i;
+    var s = c;
+    var c = s.substr(0,9);
+    var dv = s.substr(9,2);
+    var d1 = 0;
+    var v = false;
+ 
+    for (i = 0; i < 9; i++){
+        d1 += c.charAt(i)*(10-i);
+    }
+    if (d1 == 0){
+        return false;
+    }
+    d1 = 11 - (d1 % 11);
+    if (d1 > 9) d1 = 0;
+    if (dv.charAt(0) != d1){
+        return false;
+    }
+ 
+    d1 *= 2;
+    for (i = 0; i < 9; i++){
+        d1 += c.charAt(i)*(11-i);
+    }
+    d1 = 11 - (d1 % 11);
+    if (d1 > 9) d1 = 0;
+    if (dv.charAt(1) != d1){
+        return false;
+    }
+    if (!v) {
+        return true;
+    }
+    return false;
 }
 
 function refreshRiskBoxes() {
